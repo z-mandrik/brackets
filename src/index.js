6dc13653d28c4cc55bbd25f4d1ec0b450d1a06d0
mandrik.zhenya <mandrik.zhenya1@gmail.com>
2019-03-24 20:43:28 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var arr = str.split('');
+  for( let i = 0; i < arr.length; i++ ) {
+      for( let j = 0; j < bracketsConfig.length; j++ ) {
+        if( ( arr[i] == bracketsConfig[j][0] ) && ( arr[i+1] == bracketsConfig[j][1] ) ) {
+          arr.splice(i, 2);
+          i = -1; j = -1; 
+          }
+        }
+  }
+  return( arr.length == 0 )?true:false;
 }
